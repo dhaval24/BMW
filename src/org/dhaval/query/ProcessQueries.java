12195ce85515e11fd1f44f75135d95b070a3fdf9
dhaval24 <dhavaldoshi24@gmail.com>
2016-05-01 02:25:05 -0500

    Revised Commit - Junit Left

--- src/org/dhaval/query/ProcessQueries.java
+++ src/org/dhaval/query/ProcessQueries.java
@@ -22,7 +22,16 @@ public class ProcessQueries {
         this.prof = prof;
     }
 
+    /*
+    @author Dhaval Doshi
+    @version 1.0
+    @since 05-01-2016
+    @return none
+     */
     public void getStudentCountForCourse(){
+        /*
+        This method fetches the list of students enrolled in each courses
+         */
         try{
             for(ClassEnum enm: ClassEnum.values()){
                 System.out.println(enm.getName()+"    "+enm.getCount());
@@ -31,8 +40,16 @@ public class ProcessQueries {
         catch(Exception ex){}
 
     }
-
+    /*
+        @author Dhaval Doshi
+        @version 1.0
+        @since 05-01-2016
+        @return none
+     */
     public void getClassesOfStudents(){
+        /*
+        This method gets the list of classes each student is enrolled in the database
+         */
         try{
 //            if(studentId <= 0){
 //                throw new StudentIdException();
@@ -48,8 +65,18 @@ public class ProcessQueries {
         }
     }
 
-
+    /*
+    @author Dhaval Doshi
+    @version 1.0
+    @since 05-01-2016
+    @params profName: Name of professor whose course list is updated
+    @params courseName: Name of the course to be associated with professor
+    @return none
+     */
     public void updateProfessorClassList(String profName, String courseName){
+        /*
+        This method adds the particular course to the professor's course list
+         */
         Map<String,Set<ClassEnum>> classProfMap = prof.getClassProfMap();
         if(profName != null){
             if(classProfMap.containsKey(profName)){
@@ -69,8 +96,18 @@ public class ProcessQueries {
             }
         }
     }
-
+    /*
+        @author Dhaval Doshi
+        @version 1.0
+        @since 05-01-2016
+        @params studentId: Integer type field denoting the unique student ID
+        @params courseName: Name of the course which student has taken
+        @return none
+    */
     public void updateStudentCourseList(int studentId, String courseName){
+        /*
+        This method adds the course to student's course list
+         */
         Map<Integer, ArrayList<ClassEnum>> studentCourseMap = prof.getStudentCourseMap();
         if(studentId >= 0){
             if(studentCourseMap.containsKey(studentId)){
@@ -90,7 +127,18 @@ public class ProcessQueries {
             }
         }
     }
+    /*
+    @author Dhaval Doshi
+    @version 1.0
+    @since 05-01-2016
+    @params profName: Name of professor to be associated with course name
+    @params courseName: Name of the course
+    @return none
+     */
     public void updateCourseProfessors(String courseName, String profName){
+        /*
+        This method adds the name of the professor to Professor set of courseName enum.
+         */
         try{
             if(profName == null){
                 throw new ProfException();
@@ -112,16 +160,33 @@ public class ProcessQueries {
         }
 
     }
-
+    /*
+        @author Dhaval Doshi
+        @version 1.0
+        @since 05-01-2016
+        @return none
+    */
     public void getProfOfCourses(){
+        /*
+        This method gets the tuples of courses and associated professors with it
+         */
         for(ClassEnum enm: ClassEnum.values()){
             for(String profName: enm.getProfessors()){
                 System.out.println(enm.getName()+"   "+profName);
             }
         }
     }
-
+    /*
+    @author Dhaval Doshi
+    @version 1.0
+    @since 05-01-2016
+    @params courseName: Name of the course whose count is to be updated
+    @return none
+     */
     public void updateCourseCount(String courseName){
+        /*
+        This methods increases the number of students registered for a particular course
+         */
         try{
             if(courseName == null){
                 throw new NoCourseNameException();
@@ -134,8 +199,16 @@ public class ProcessQueries {
         }
 
     }
-
+    /*
+        @author Dhaval Doshi
+        @version 1.0
+        @since 05-01-2016
+        @return none
+         */
     public void getStudentsWithMoreClasses(){
+        /*
+        This method returns the id of the students who are enrolled in more than 1 classes
+         */
         Map<Integer, ArrayList<ClassEnum>> studentCourseMap = prof.getStudentCourseMap();
         for(Map.Entry items: studentCourseMap.entrySet()){
             if(((ArrayList)items.getValue()).size() > 1){
@@ -143,8 +216,16 @@ public class ProcessQueries {
             }
         }
     }
-
+    /*
+        @author Dhaval Doshi
+        @version 1.0
+        @since 05-01-2016
+        @return none
+         */
     public void getProfessorWithMoreClasses(){
+        /*
+        This method returns the name of the professor who teaches more than 1 course
+         */
         Map<String,Set<ClassEnum>> classProfMap = prof.getClassProfMap();
         for(Map.Entry items: classProfMap.entrySet()){
             if(((HashSet)items.getValue()).size() > 1){
